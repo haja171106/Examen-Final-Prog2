d58bf360e7f008ebc2b1bb859e828481ea8a85c5
haja <hei.haja.@gmail.com>
2025-09-04 10:04:31 +0300

    add beforeEach in test

--- src/test/java/note/ExamenTest.java
+++ src/test/java/note/ExamenTest.java
@@ -3,11 +3,29 @@ package note;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.Instant;
+import java.util.List;
+
 public class ExamenTest {
     @BeforeEach
+    public void setUp() {
+        Examen examen;
+        Etudiant etudiant;
+        Enseignant enseignant;
+        Cours cours;
 
+        enseignant = new Enseignant(1, "Toky", "dr", Instant.parse("1980-01-01T00:00:00Z"), "toky@mail.hei.school", "0340000000", "back-end");
+        cours = new Cours(1, "prog 2", 5, enseignant);
+        examen = new Examen(1, "prog 2", cours, Instant.parse("2023-06-01T10:00:00Z"), 2, List.of());
+        etudiant = new Etudiant(1, "haja", "rvh", Instant.parse("2000-01-01T00:00:00Z"), "rvh@gmail.com", "0123456789", "k4", null);
+
+
+    }
     @Test
     public void testGetExamGrade() {
+        assertEquals(0, 0);
     }
 
     @Test
